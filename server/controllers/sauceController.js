e638c7ebcd72729ca1be70210a349edd29784f97
dbiddings444 <dbiddings444@gmail.com>
2024-07-14 18:47:06 -0400

    changes

--- server/controllers/sauceController.js
+++ server/controllers/sauceController.js
@@ -1,5 +1,5 @@
 const Sauce = require("../models/sauce");
-const user = require("../models/user");
+const User = require("../models/user"); // Use capital U for model imports
 const dotenv = require("dotenv");
 const fs = require('fs');
 const path = require('path');
@@ -7,158 +7,156 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: ".env" });
 
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then((sauces) => {
-      const mappedProducts = sauces.map((sauce) => {
-        return sauce;
-      });
-      res.status(200).json(mappedProducts);
-    })
-    .catch(() => {
-      res.status(500).send(new Error("Database error!"));
-    });
+// Get all sauces
+exports.getAllSauces = async (req, res, next) => {
+  try {
+    const sauces = await Sauce.find();
+    res.status(200).json(sauces);
+  } catch (error) {
+    res.status(500).json({ message: "Database error!" });
+  }
 };
 
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findById(req.params.id)
-    .then((sauce) => {
-      if (!sauce) {
-        return res.status(404).send(new Error("Sauce not found!"));
+// Get one sauce
+exports.getOneSauce = async (req, res, next) => {
+  try {
+    const sauce = await Sauce.findById(req.params.id);
+    if (!sauce) {
+      return res.status(404).json({ message: "Sauce not found!" });
+    }
+    res.status(200).json(sauce);
+  } catch (error) {
+    res.status(500).json({ message: "Database error!" });
+  }
+};
+
+// Delete a sauce
+exports.deleteSauce = async (req, res, next) => {
+  try {
+    const sauce = await Sauce.findById(req.params.id);
+    if (!sauce) {
+      return res.status(404).json({ message: "Sauce not found!" });
+    }
+
+    const filename = sauce.imageUrl.split("/uploads/")[1];
+    fs.unlink(path.join("uploads", filename), async (err) => {
+      if (err) {
+        return res.status(500).json({ message: "File deletion error!" });
       }
-      res.status(200).json(sauce);
-    })
-    .catch(() => {
-      res.status(500).send(new Error("Database error!"));
-    });
-};
 
-exports.deleteSauce = (req, res, next) => {
-  Sauce.findById(req.params.id)
-    .then((sauce) => {
-      if (!sauce) {
-        return res.status(404).json({ error: "Sauce not found!" });
+      try {
+        await Sauce.deleteOne({ _id: req.params.id });
+        res.status(200).json({ message: "Sauce deleted successfully!" });
+      } catch (error) {
+        res.status(400).json({ message: error.message });
       }
-
-      const filename = sauce.imageUrl.split("/uploads/")[1];
-
-      fs.unlink(path.join( "uploads", filename), (err) => {
-        if (err) {
-          return res.status(500).json({ error: "File deletion error!" });
-        }
-
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() =>
-            res.status(200).json({ message: "Sauce deleted successfully!" })
-          )
-          .catch((error) => res.status(400).json({ error: error.message }));
-      });
-    })
-    .catch((error) => res.status(500).json({ error: "Database error!" }));
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Database error!" });
+  }
 };
 
+// Create a sauce
+exports.createSauce = async (req, res) => {
+  try {
+    const sauceObject = JSON.parse(req.body.sauce);
+    const userId = req.userData.userId;
 
-exports.createSauce = (req, res) => {
-  // Parse the stringified sauce data
-  const sauceObject = JSON.parse(req.body.sauce);
-  const userId = req.userData.userId;
-  // Construct the sauce object
-  const sauce = new Sauce({
-    ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get("host")}/uploads/${
-      req.file.filename
-    }`,
-    userId: new mongoose.Types.ObjectId(userId),
-    likes: 0,
-    dislikes: 0,
-    usersLiked: [],
-    usersDisliked: [],
-  });
-  sauce
-    .save()
-    .then(() =>
-      res.status(201).json({ message: "Sauce created successfully!" })
-    )
-    .catch((error) => res.status(400).json({ error: error.message }));
+    const sauce = new Sauce({
+      ...sauceObject,
+      imageUrl: `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`,
+      userId: userId,
+      likes: 0,
+      dislikes: 0,
+      usersLiked: [],
+      usersDisliked: []
+    });
+
+    await sauce.save();
+    res.status(201).json({ message: "Sauce created successfully!" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
-exports.updateSauce = (req, res, next) => {
-  // Parse the request body to get sauceObject
-  const sauceObject = req.file ? {
+// Update a sauce
+exports.updateSauce = async (req, res, next) => {
+  try {
+    const sauceObject = req.file ? {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
-  } : { ...req.body };
+    } : { ...req.body };
 
-  // Find the sauce by ID
-  Sauce.findById(req.params.id).then(sauce => {
-      console.log(sauceObject, "the code got this far");
+    const sauce = await Sauce.findById(req.params.id);
 
-      if (!sauce) {
-          return res.status(404).json({ message: 'Sauce not found!' });
+    if (!sauce) {
+      return res.status(404).json({ message: 'Sauce not found!' });
+    }
+
+    if (sauce.userId.toString() !== req.userData.userId) {
+      return res.status(403).json({ message: 'Unauthorized request!' });
+    }
+
+    const updateSauceInDatabase = async () => {
+      try {
+        await Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id });
+        res.status(200).json({ message: 'Sauce updated successfully!' });
+      } catch (error) {
+        res.status(400).json({ message: error.message });
       }
+    };
 
-      if (sauce.userId !== req.userData.userId) {
-          return res.status(403).json({ message: 'Unauthorized request!' });
-      }
-
-      const updateSauceInDatabase = () => {
-          Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-              .then(() => res.status(200).json({ message: 'Sauce updated successfully!' }))
-              .catch(error => res.status(400).json({ error }));
-      };
-
-      if (req.file && sauce.imageUrl) {
-          console.log(sauceObject, "the code got this far part 3");
-          const filename = sauce.imageUrl.split('/uploads/')[1];
-          fs.unlink(`uploads/${filename}`, (err) => {
-              if (err) {
-                  console.error('File deletion error:', err);
-                  return res.status(500).json({ error: 'File deletion error!' });
-              }
-              updateSauceInDatabase();
-          });
-      } else {
-          console.log(sauceObject, "the code got this far part 3");
-          updateSauceInDatabase();
-      }
-  }).catch(error => {
-      console.error('Database error:', error);
-      res.status(500).json({ error: 'Database error!' });
-  });
+    if (req.file && sauce.imageUrl) {
+      const filename = sauce.imageUrl.split('/uploads/')[1];
+      fs.unlink(path.join("uploads", filename), (err) => {
+        if (err) {
+          console.error('File deletion error:', err);
+          return res.status(500).json({ message: 'File deletion error!' });
+        }
+        updateSauceInDatabase();
+      });
+    } else {
+      updateSauceInDatabase();
+    }
+  } catch (error) {
+    console.error('Database error:', error);
+    res.status(500).json({ message: 'Database error!' });
+  }
 };
 
-exports.likes = (req, res, next) => {
-  const userId = req.userData.userId;
+// Update likes and dislikes for a sauce
+exports.likes = async (req, res, next) => {
+  try {
+    const userId = req.userData.userId;
     const like = req.body.like;
 
-    Sauce.findById(req.params.id).then((sauce) => {
-        if (!sauce) {
-            return res.status(404).json({ message: 'Sauce not found!' });
-        }
+    const sauce = await Sauce.findById(req.params.id);
+    if (!sauce) {
+      return res.status(404).json({ message: 'Sauce not found!' });
+    }
 
-        // Remove user from both arrays to handle updating like/dislike
-        if (sauce.usersLiked.includes(userId)) {
-            sauce.usersLiked.pull(userId);
-            sauce.likes -= 1;
-        }
-        if (sauce.usersDisliked.includes(userId)) {
-            sauce.usersDisliked.pull(userId);
-            sauce.dislikes -= 1;
-        }
+    // Remove user from both arrays to handle updating like/dislike
+    if (sauce.usersLiked.includes(userId)) {
+      sauce.usersLiked.pull(userId);
+      sauce.likes -= 1;
+    }
+    if (sauce.usersDisliked.includes(userId)) {
+      sauce.usersDisliked.pull(userId);
+      sauce.dislikes -= 1;
+    }
 
-        // Update arrays and counts based on new like value
-        if (like === 1) {
-            sauce.usersLiked.push(userId);
-            sauce.likes += 1;
-        } else if (like === -1) {
-            sauce.usersDisliked.push(userId);
-            sauce.dislikes += 1;
-        }
+    // Update arrays and counts based on new like value
+    if (like === 1) {
+      sauce.usersLiked.push(userId);
+      sauce.likes += 1;
+    } else if (like === -1) {
+      sauce.usersDisliked.push(userId);
+      sauce.dislikes += 1;
+    }
 
-        sauce.save()
-            .then(() => res.status(200).json({ message: 'Sauce like status updated successfully!' }))
-            .catch((error) => res.status(500).json({ message: 'Failed to update sauce like status!', error }));
-    }).catch((error) => res.status(500).json({ message: 'Database error!', error }));
-
-  
+    await sauce.save();
+    res.status(200).json({ message: 'Sauce like status updated successfully!' });
+  } catch (error) {
+    res.status(500).json({ message: 'Database error!', error });
+  }
 }
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,52 +7,63 @@ const mongoose = require('mongoose');
 dotenv.config({ path: '.env' });
 const jwtSecret = process.env.JWT_SECRET_KEY;
 
-function encryptPassword(response, user) {
-	bcrypt.genSalt(10, (err, salt) => {
-		bcrypt.hash(user.password, salt, (err, hash) => {
-			if (err) throw err;
-			user.password = hash;
-			user.save()
-				.then(user => {
-					response.send({ message: 'Registration successful!' })
-				})
-				.catch(err => console.log(err));
-		});
-	});
+async function encryptPassword(response, user) {
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(user.password, salt);
+		user.password = hash;
+		await user.save();
+		response.send({ message: 'Registration successful!' });
+	} catch (err) {
+		console.error(err);
+		response.status(500).send({ message: 'Error during registration' });
+	}
 }
 
-exports.signup = (req, res) => {
-	console.log(req.body);
-	const { email, password } = req.body;
-	User.findOne({ email: email })
-		.then(user => {
-			if (user) {
-				res.status(400).json({ message: 'Email already exists' });
-			} else {
-				const newUser = new User({ email, password });
-				encryptPassword(res, newUser);
-			}
-		})
+exports.signup = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return res.status(400).json({ message: 'Email and password are required' });
+		}
+
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: 'Email already exists' });
+		}
+
+		const newUser = new User({ email, password });
+		await encryptPassword(res, newUser);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: 'Error during signup' });
+	}
 }
 
-exports.login = (req, res) => {
-	console.log(req.body);
-	const { email, password } = req.body;
-	User.findOne({ email: email })
-		.then(user => {
-			if (!user) {
-				res.send({ msessage: 'Email not found' });
-			} else {
-				bcrypt.compare(password, user.password, (err, matches) => {
-					if (matches) {
-						const signedToken = jwt.sign({ email: email, userId:user._id }, jwtSecret, { expiresIn: '1h' })
-						console.log(signedToken);
-						console.log('userId: ',user._id)
-						res.send({ token: signedToken });
-					}
-				})
-			}
-		})
+exports.login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return res.status(400).json({ message: 'Email and password are required' });
+		}
+
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ message: 'Email not found' });
+		}
+
+		const matches = await bcrypt.compare(password, user.password);
+		if (matches) {
+			const signedToken = jwt.sign({ email, userId: user._id.toString() }, jwtSecret, { expiresIn: '1h' });
+			res.send({ token: signedToken });
+			console.log('userId',user._id.toString());
+		} else {
+			res.status(401).json({ message: 'Incorrect password' });
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: 'Error during login' });
+	}
 }
 
 exports.verify = (req, res, next) => {
@@ -61,6 +72,7 @@ exports.verify = (req, res, next) => {
 		if (!token) {
 			return res.status(401).json({ message: 'Auth token is missing!' });
 		}
+
 		const decodedToken = jwt.verify(token, jwtSecret);
 		req.userData = { userId: decodedToken.userId };
 		next();
@@ -68,10 +80,6 @@ exports.verify = (req, res, next) => {
 		res.status(401).json({ message: 'Auth failed!' });
 	}
 }
-
-
-
-
 
 exports.authenticated = (req, res) => {
 	res.send('Authenticated page');
